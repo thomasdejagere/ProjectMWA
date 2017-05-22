b5ab5ee1f59d8f428ebd9485ff45f7c012f3e4c6
Thomas Dejagere <thomasdejagere@gmail.com>
2017-05-22 16:20:37 +0200

    First pages commit

--- app/globalConstants.js
+++
@@ -1,1 +0,0 @@
-export const REST_API = "http://192.168.1.88:4444";
--- app/utils/index.js
+++
@@ -1,6 +0,0 @@
-export function prefixObjectValues(object, prefix) {
-  return Object.keys(object).reduce(function (result, key) {
-    result[key] = prefix + object[key];
-    return result;
-  }, {});
-}
